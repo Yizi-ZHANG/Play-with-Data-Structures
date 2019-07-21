cb56fd1fc9a1799a8f74df6a20e18f0ddfbeef45
Zhang Kent <46770323+Yizi-ZHANG@users.noreply.github.com>
2019-07-21 09:18:02 +0800

    Dynamic-Int-Array
    Add files by uploading for GitHub web is too ugly

---
+++ 02-Arrays/Dynamic-Int-Array/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        Array arr = new Array(10);
+        for(int i = 0; i < 10; i ++)
+            arr.addLast(i);
+        System.out.println(arr);
+
+        // 复杂度震荡
+        arr.add(2,100);
+        System.out.println(arr);
+
+        arr.remove(5);
+        System.out.println(arr);
+
+        arr.addLast(1);
+        arr.addFirst(-1);
+        System.out.println(arr);
+
+        arr.removeElement(5);
+        arr.removeFirst();
+        arr.removeLast();
+        System.out.println(arr);
+    }
+}
